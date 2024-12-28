3a972133976b18a0b42553f499eb271b14d9bf2f
polluterofminds <polluterofminds@Mac.attlocal.net>
2024-12-28 16:51:12 -0600

    fixed build error

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
