5ab99b5d4fa5a92bde95190c11db641fe3bf064e
Steve <stevedsimkins@gmail.com>
2024-12-29 11:25:16 -0500

    fix: Changed from process env to import meta

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -17,7 +17,7 @@ export async function getUserLocal(): Promise<Session | null> {
 
 const getURL = () => {
 	let url =
-		process?.env?.VITE_SITE_URL ?? // Set this to your site URL in production env.
+		import.meta.env.VITE_SITE_URL ?? // Set this to your site URL in production env.
 		"http://localhost:3000/";
 	// Make sure to include `https://` when not localhost.
 	url = url.startsWith("http") ? url : `https://${url}`;
