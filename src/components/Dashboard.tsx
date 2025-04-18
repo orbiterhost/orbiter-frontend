4d439acf92206e79b34f34d4a4b391e5abc6c841
Steve <stevedsimkins@gmail.com>
2025-04-17 20:19:29 -0400

    feat: Added new empty state dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,10 +5,20 @@ import { useToast } from "@/hooks/use-toast";
 import { getAccessToken } from "@/utils/auth";
 import { Organization, Site } from "@/utils/types";
 import { useEffect, useState } from "react";
-import { Card } from "./ui/card";
 import { Button } from "./ui/button";
-import { RocketIcon } from "lucide-react";
+import { Check, Copy } from "lucide-react";
 import { UpsellModal } from "./upsell-modal";
+import {
+  Stepper,
+  StepperDescription,
+  StepperIndicator,
+  StepperItem,
+  StepperSeparator,
+  StepperTitle,
+  StepperTrigger,
+} from "@/components/ui/stepper"
+import APIKeyCreator from "./api-key-creator";
+
 
 type DashboardProps = {
   organization: any;
@@ -26,7 +36,17 @@ type DashboardProps = {
 
 const Dashboard = (props: DashboardProps) => {
   const [selectedTemplateCid, setSelectedTemplateCid] = useState("");
+  const [copiedField, setCopiedField] = useState<string>("");
+
   let maxSites: any;
+
+  const handleCopy = (text: string, field: string) => {
+    navigator.clipboard.writeText(text);
+    setCopiedField(field);
+    setTimeout(() => {
+      setCopiedField("");
+    }, 2000);
+  };
 
   useEffect(() => {
     setSelectedTemplateCid("");
@@ -122,50 +142,10 @@ const Dashboard = (props: DashboardProps) => {
         ) : (
           <CreateSiteForm {...props} />
         )}
-        <Button className="hidden sm:flex" asChild>
-          <a href="https://voyager.orbiter.host" target="_blank">
-            <RocketIcon />
-            Voyager
-          </a>
-        </Button>
       </div>
       {!props.initialLoading && props.sites.length === 0 && (
         <>
-          <div className="w-full flex justify-end px-24">
-            <p>No sites 🥸 upload one now!</p>
-            <svg
-              width="88"
-              height="165"
-              viewBox="0 0 88 165"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M81.6025 1.81033C80.0047 2.69652 75.6006 5.48933 71.8276 8.04047C59.9043 16.0967 54.7483 19.4132 47.2156 23.9515C43.1741 26.3684 39.6159 28.5839 39.2936 28.839C38.4074 29.6178 38.6759 31.4439 39.777 32.2629C40.596 32.8537 40.784 32.8806 42.0596 32.518C43.8588 31.9944 51.1901 28.3154 55.8493 25.6434C57.8365 24.5289 59.5686 23.5219 59.7297 23.4279C59.8909 23.3339 58.2259 25.3211 56.0104 27.7917C43.5634 41.9036 35.0506 54.2967 27.0077 70.0332C19.9048 83.9571 15.3127 95.1955 10.0224 111.684C6.07482 123.943 3.92648 133.879 1.4156 150.931C-0.0479595 160.908 -0.276188 163.674 0.274325 164.332C0.865119 165.043 1.48273 164.922 2.12724 163.969C2.6509 163.15 2.65091 163.083 2.1004 162.693C1.54988 162.264 1.54989 162.035 2.23468 157.255C3.12087 150.703 5.33634 137.639 6.34338 132.778C7.45783 127.595 11.4994 113.577 13.97 106.313C23.8524 77.7269 36.2054 55.2366 52.9222 35.5794C59.4075 27.9528 71.7336 15.2374 73.6134 14.2304C74.2982 13.8678 74.2982 13.9081 73.5194 15.4388C71.9216 18.5942 68.7662 26.2341 67.8129 29.2284C66.6984 32.6791 65.0737 39.9566 63.4759 48.3619C62.5897 52.954 62.4689 54.1356 62.7911 54.8204C63.5028 56.3914 66.0136 56.6196 66.9267 55.1829C67.1549 54.8204 67.8397 52.1484 68.3902 49.2884C71.9484 31.2693 73.5463 26.543 80.4881 13.0085C82.2471 9.51746 83.7106 6.52321 83.7106 6.32181C83.7106 6.09355 84.5968 5.38191 85.671 4.72398C87.8596 3.35442 88.3698 2.37426 87.4971 1.04498C86.6512 -0.257445 85.0533 -0.0694435 81.6025 1.82377V1.81033Z"
-                fill="currentColor"
-              />
-            </svg>
-          </div>
           <div className="w-3/4 md:w-4/5 lg:w-full m-auto flex flex-col items-center gap-4">
-            <h3 className="text-2xl">
-              Need a jumpstart? Check out{" "}
-              <span className="font-bold uppercase">Voyager</span>
-            </h3>
-            <a href="https://voyager.orbiter.host" target="_blank">
-              <Card className="overflow-hidden max-w-xl">
-                <img
-                  src="https://cdn.orbiter.host/ipfs/bafkreiajqerzvsfzjt4pumsqh3ntairipitvc7ozeqwrucrnaiqhyzaahu"
-                  alt="voyager cover"
-                />
-              </Card>
-            </a>
-            <Button asChild>
-              <a href="https://voyager.orbiter.host" target="_blank">
-                Launch
-              </a>
-            </Button>
             {selectedTemplateCid && (
               <CreateSiteForm
                 {...props}
@@ -173,28 +153,95 @@ const Dashboard = (props: DashboardProps) => {
                 setSelectedTemplateCid={setSelectedTemplateCid}
               />
             )}
-            <div className="mt-8">
-              <p>
-                Alternatively deploy your site with our{" "}
-                <a
-                  href="https://docs.orbiter.host/cli"
-                  className="underline"
-                  target="_blank"
+            <div className="mt-8 space-y-8">
+              <Stepper defaultValue={2} orientation="vertical">
+                <StepperItem
+                  key={1}
+                  step={1}
+                  className="relative items-start not-last:flex-1"
                 >
-                  CLI
-                </a>{" "}
-                +{" "}
-                <a href="/api-keys" className="underline">
-                  API Key!
-                </a>
-              </p>
-              <pre className="font-mono my-2 p-4 bg-gray-100 rounded-md text-sm max-w-md">
-                <code>
-                  npm i -g orbiter-cli <br />
-                  orbiter auth <br />
-                  orbiter deploy
-                </code>
-              </pre>
+                  <StepperTrigger className="items-start rounded pb-12 last:pb-0">
+                    <StepperIndicator />
+                    <div className="mt-0.5 space-y-0.5 px-2 text-left">
+                      <StepperTitle>Create an API Key</StepperTitle>
+                      <StepperDescription>
+                        Click the button below to make a new API key and save it somewhere safe
+                        <APIKeyCreator />
+                      </StepperDescription>
+                    </div>
+                  </StepperTrigger>
+                  <StepperSeparator className="absolute inset-y-0 top-[calc(1.5rem+0.125rem)] left-3 -order-1 m-0 -translate-x-1/2 group-data-[orientation=horizontal]/stepper:w-[calc(100%-1.5rem-0.25rem)] group-data-[orientation=horizontal]/stepper:flex-none group-data-[orientation=vertical]/stepper:h-[calc(100%-1.5rem-0.25rem)]" />
+                </StepperItem>
+                <StepperItem
+                  key={2}
+                  step={2}
+                  className="relative items-start not-last:flex-1"
+                >
+                  <StepperTrigger className="items-start rounded pb-12 last:pb-0">
+                    <StepperIndicator />
+                    <div className="mt-0.5 space-y-0.5 px-2 text-left">
+                      <StepperTitle>Install the CLI</StepperTitle>
+                      <StepperDescription>
+                        Run the following command in your terminal to install the Orbiter CLI
+                        <div className="flex items-center justify-between p-2 mt-4 bg-gray-100 rounded">
+                          <code className="font-mono text-sm truncate max-w-[300px]">npm i -g orbiter-cli</code>
+                          <Button variant="ghost" size="sm" onClick={() => handleCopy('npm i -g orbiter-cli', 'key')}>
+                            {copiedField === 'key' ? <Check className="h-4 w-4" /> : <Copy className="h-4 w-4" />}
+                          </Button>
+                        </div>
+                      </StepperDescription>
+                    </div>
+                  </StepperTrigger>
+                  <StepperSeparator className="absolute inset-y-0 top-[calc(1.5rem+0.125rem)] left-3 -order-1 m-0 -translate-x-1/2 group-data-[orientation=horizontal]/stepper:w-[calc(100%-1.5rem-0.25rem)] group-data-[orientation=horizontal]/stepper:flex-none group-data-[orientation=vertical]/stepper:h-[calc(100%-1.5rem-0.25rem)]" />
+                </StepperItem>
+
+                <StepperItem
+                  key={3}
+                  step={3}
+                  className="relative items-start not-last:flex-1"
+                >
+                  <StepperTrigger className="items-start rounded pb-12 last:pb-0">
+                    <StepperIndicator />
+                    <div className="mt-0.5 space-y-0.5 px-2 text-left">
+                      <StepperTitle>Authorize the CLI</StepperTitle>
+                      <StepperDescription>
+                        Run the command below and paste in your API key when prompted
+                        <div className="flex items-center justify-between p-2 mt-4 bg-gray-100 rounded">
+                          <code className="font-mono text-sm truncate max-w-[300px]">orbiter auth</code>
+                          <Button variant="ghost" size="sm" onClick={() => handleCopy('orbiter auth', 'auth')}>
+                            {copiedField === 'auth' ? <Check className="h-4 w-4" /> : <Copy className="h-4 w-4" />}
+                          </Button>
+                        </div>
+                      </StepperDescription>
+                    </div>
+                  </StepperTrigger>
+                  <StepperSeparator className="absolute inset-y-0 top-[calc(1.5rem+0.125rem)] left-3 -order-1 m-0 -translate-x-1/2 group-data-[orientation=horizontal]/stepper:w-[calc(100%-1.5rem-0.25rem)] group-data-[orientation=horizontal]/stepper:flex-none group-data-[orientation=vertical]/stepper:h-[calc(100%-1.5rem-0.25rem)]" />
+                </StepperItem>
+                <StepperItem
+                  key={4}
+                  step={4}
+                  className="relative items-start not-last:flex-1"
+                >
+                  <StepperTrigger className="items-start rounded pb-12 last:pb-0">
+                    <StepperIndicator />
+                    <div className="mt-0.5 space-y-0.5 px-2 text-left">
+                      <StepperTitle>Creat a New App</StepperTitle>
+                      <StepperDescription>
+                        Run the command below to start a new Orbiter app
+                        <div className="flex items-center justify-between p-2 mt-4 bg-gray-100 rounded">
+                          <code className="font-mono text-sm truncate max-w-[300px]">orbiter new</code>
+                          <Button variant="ghost" size="sm" onClick={() => handleCopy('orbiter new', 'new')}>
+                            {copiedField === 'new' ? <Check className="h-4 w-4" /> : <Copy className="h-4 w-4" />}
+                          </Button>
+                        </div>
+                      </StepperDescription>
+                    </div>
+                  </StepperTrigger>
+                  <StepperSeparator className="absolute inset-y-0 top-[calc(1.5rem+0.125rem)] left-3 -order-1 m-0 -translate-x-1/2 group-data-[orientation=horizontal]/stepper:w-[calc(100%-1.5rem-0.25rem)] group-data-[orientation=horizontal]/stepper:flex-none group-data-[orientation=vertical]/stepper:h-[calc(100%-1.5rem-0.25rem)]" />
+                </StepperItem>
+
+              </Stepper>
+
             </div>
           </div>
         </>
---
+++ src/components/api-key-creator.tsx
@@ -0,0 +1,185 @@
+import { getAccessToken } from "@/utils/auth";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { useToast } from "@/hooks/use-toast";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Check, Copy, Loader2, PlusIcon } from "lucide-react";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+
+
+type KeySecret = {
+  name: string;
+  scope: "Admin" | "Member";
+  apiKey: string;
+}
+
+const APIKeyCreator = () => {
+
+  const [loading, setLoading] = useState(false)
+  const [keyScope, setKeyScope] = useState("")
+  const [keyName, setKeyName] = useState("")
+  const [keyCreated, setKeyCreated] = useState(false)
+  const [tempKey, setTempKey] = useState<KeySecret | null>(null)
+  const [open, setOpen] = useState(false)
+
+  const [copiedField, setCopiedField] = useState<string>("");
+
+  const handleCopy = (text: string, field: string) => {
+    navigator.clipboard.writeText(text);
+    setCopiedField(field);
+    setTimeout(() => {
+      setCopiedField("");
+    }, 2000);
+  };
+
+
+  const { toast } = useToast();
+
+  const createAPIKey = async () => {
+    try {
+      setLoading(true)
+
+      if (keyName.length === 0) {
+        toast({
+          title: "Must provide a key name",
+          variant: 'destructive'
+        })
+        return
+      }
+      if (keyScope !== "Admin" && keyScope !== "Member") {
+        toast({
+          title: "Must select either Admin or Member scope",
+          variant: 'destructive'
+        })
+        return
+      }
+
+      const accessToken = await getAccessToken();
+      const res = await fetch(
+        `${import.meta.env.VITE_BASE_URL}/keys`,
+        {
+          method: "POST",
+          headers: {
+            "X-Orbiter-Token": `${accessToken}`,
+          },
+          body: JSON.stringify({
+            name: keyName,
+            scope: keyScope
+          })
+        }
+      );
+      const data = await res.json()
+      setTempKey(data)
+      if (!res.ok) {
+        console.log(data)
+        toast({
+          title: "Problem creating API Key",
+          description: data.toString(),
+          variant: 'destructive'
+        })
+        setLoading(false)
+        return
+      }
+      setLoading(false)
+      setKeyCreated(true)
+      setKeyScope("")
+      setKeyName("")
+    } catch (error) {
+      console.log(error)
+      toast({
+        title: "Problem creating API Key",
+        description: `${error}`,
+        variant: 'destructive'
+      })
+      setLoading(false)
+      setKeyCreated(false)
+    }
+  };
+
+
+
+  return (
+    <div className="sm:max-w-screen-lg max-w-screen-sm w-full sm:mx-auto mx-4 flex flex-col items-start justify-center gap-2">
+      <Dialog
+        onOpenChange={(open) => {
+          setOpen(open);
+          if (!open) {
+            setKeyCreated(false);
+            setTempKey(null)
+          }
+        }}
+        open={open}
+      >
+        <div className="w-full flex items-center justify-start mt-6">
+          <DialogTrigger>
+            <Button>
+              <PlusIcon />
+              New Key
+            </Button>
+          </DialogTrigger>
+        </div>
+        <DialogContent
+          onPointerDownOutside={(e) => {
+            e.preventDefault();
+          }}
+        >
+          {!keyCreated ? (
+            <>
+              <DialogHeader>
+                <DialogTitle>Create New API Key</DialogTitle>
+                <DialogDescription>Select a scope and provide a name</DialogDescription>
+              </DialogHeader>
+              <Select onValueChange={(value) => setKeyScope(value)}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Scope" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="Member">Member</SelectItem>
+                  <SelectItem value="Admin">Admin</SelectItem>
+                </SelectContent>
+              </Select>
+              <Input
+                type="text"
+                placeholder="My Key"
+                value={keyName}
+                onChange={(e) => setKeyName(e.target.value)}
+              />
+              {loading ?
+                <Button disabled>
+                  <Loader2 className="animate-spin" />
+                  Creating...
+                </Button>
+                :
+                <Button onClick={createAPIKey}>Create</Button>
+              }
+            </>
+          ) : (
+            <>
+              <DialogHeader>
+                <DialogTitle>Copy Your Key Info</DialogTitle>
+                <DialogDescription>This screen is only shown once!</DialogDescription>
+              </DialogHeader>
+              <Label>Secret Key</Label>
+              <div className="flex items-center justify-between p-2 bg-gray-100 rounded">
+                <code className="font-mono text-sm truncate max-w-[300px]">{tempKey?.apiKey}</code>
+                <Button variant="ghost" size="sm" onClick={() => handleCopy(tempKey?.apiKey as string, 'key')}>
+                  {copiedField === 'key' ? <Check className="h-4 w-4" /> : <Copy className="h-4 w-4" />}
+                </Button>
+              </div>
+            </>
+          )}
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default APIKeyCreator;
